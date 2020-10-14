aac187d27c6572c8683b3b9903a5bd6ed9f3ecc6
EK <ekim0480@gmail.com>
2020-10-14 12:15:56 -0500

    functional html routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+const notesDB = require('./db/db.json')
 
 // server configuration
 const app = express()
@@ -12,17 +13,17 @@ app.use(express.static('public'))
 
 
 
-// variable to parse data
-const readDB = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json'), (err, data) => {
-    if (err) console.log(error)
-}))
 
-// variable to write data
-const writeDB = function(note) {
-    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(note), (err, data) => {
-        if (err) console.log(err)
-    })
-}
+// const readNote = fs.readFileSync(__dirname, notes, (err, data) => {
+//     if (err) console.log(error)
+// })
+
+
+// const writeNote = function(note) {
+//     fs.writeFileSync(__dirname, notes), (err, data) => {
+//         if (err) console.log(err)
+//     }
+// }
 
 // html routes
 
@@ -40,7 +41,7 @@ app.get('/notes', function (req, res) {
 
 // GET all notes
 app.get('/api/notes', function (req, res) {
-    res.json(fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf8'))
+    res.json(notesDB)
 })
 
 // POST note
@@ -49,10 +50,16 @@ app.post('/api/notes', function (req, res) {
     // assigning an id so we can delete by id later
     // +1 to make it simple so first post starts at 1 instead of 0
     newPost.id = readDB.length + 1
-    readDB.push(newPost)
-    writeDB(readDB)
-    return res.json(readDB)
+    readNotes.push(newPost)
+    writeNotes(readNotes)
+    return res.json(readNotes)
 })
+
+// fs.readFileSync(path.join(__dirname, './db/db.json'), (err, data) => {
+//     if (err) console.log(error)
+
+//     fs.writeFile(noteFile, JSON.stringify(currentNotes), (err) => { if (err) throw err;
+//         res.json(currentNotes);
 
 // DELETE note
 app.delete('/api/notes/:id', function(req, res) {
@@ -60,8 +67,8 @@ app.delete('/api/notes/:id', function(req, res) {
     readDB = readDB.filter((note) => {
         return note.id != deleteID
     })
-    writeDB(readDB)
-    return res.json(readDB)
+    writeNotes(readNotes)
+    return res.json(readNotes)
 })
 
 // start server, begins listening..
