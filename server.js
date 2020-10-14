a1c9618aecf623f7e4b6ab0f9f8218ff0c73ba95
EK <ekim0480@gmail.com>
2020-10-14 13:09:36 -0500

    post working

--- server.js
+++ server.js
@@ -11,25 +11,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
 
-
-
-
-// const readNote = fs.readFileSync(__dirname, notes, (err, data) => {
-//     if (err) console.log(error)
-// })
-
-
-// const writeNote = function(note) {
-//     fs.writeFileSync(__dirname, notes), (err, data) => {
-//         if (err) console.log(err)
-//     }
-// }
-
 // html routes
 
 // default page
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+    res.sendFile(__dirname, './public/index.html')
 })
 
 // notes page
@@ -49,27 +35,35 @@ app.post('/api/notes', function (req, res) {
     let newPost = req.body
     // assigning an id so we can delete by id later
     // +1 to make it simple so first post starts at 1 instead of 0
-    newPost.id = readDB.length + 1
-    readNotes.push(newPost)
-    writeNotes(readNotes)
-    return res.json(readNotes)
+    newPost.id = notesDB.length + 1
+    // read data from db
+    fs.readFile('./db/db.json', "utf8", (err, data) =>{
+        if (err) console.log(err)
+        console.log(data)
+        // parse into object
+        let note = JSON.parse(data)
+        console.log('parsed', note)
+        console.log('stringify', note)
+        note.push(newPost)
+        // stringify then write to db
+        note = JSON.stringify(note)
+        
+        fs.writeFileSync('./db/db.json', note, function(err) {
+            if (err) console.log(err)
+            res.json(note)
+        })
+    })
 })
-
-// fs.readFileSync(path.join(__dirname, './db/db.json'), (err, data) => {
-//     if (err) console.log(error)
-
-//     fs.writeFile(noteFile, JSON.stringify(currentNotes), (err) => { if (err) throw err;
-//         res.json(currentNotes);
 
 // DELETE note
-app.delete('/api/notes/:id', function(req, res) {
-    let deleteID = req.params.id
-    readDB = readDB.filter((note) => {
-        return note.id != deleteID
-    })
-    writeNotes(readNotes)
-    return res.json(readNotes)
-})
+// app.delete('/api/notes/:id', function(req, res) {
+//     let deleteID = req.params.id
+//     readDB = readDB.filter((note) => {
+//         return note.id != deleteID
+//     })
+//     writeNotes(readNotes)
+//     return res.json(readNotes)
+// })
 
 // start server, begins listening..
 app.listen(PORT, () => {
